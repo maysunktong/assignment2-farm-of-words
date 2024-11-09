0eed294edb15c05062323d520398778de96d0105
May Sunktong <pattaranan.sunktong@gmail.com>
2024-11-09 21:57:29 +0100

    Create playGame() and move outside startGame() to be able to replay it after evaluation

--- script/game.js
+++ script/game.js
@@ -14,108 +14,28 @@ const LETTER_REGEX = /^[a-z]+$/i;
 const MAX_WORD_LENGTH = 5;
 let leftAttempts = 6;
 
-const startGame = () => {
-  // promptName starts
-  let promptName = prompt(
-    `🧙🏻‍♂️[Mayor]: Hi. My name is Thomas Morot, Mayor of Grönville. What is your name?`
-  );
-
-  if (promptName === null) {
-    alert("You cancelled the process. Exiting game...");
-    return;
-  }
-
-  if (promptName.trim() === "") {
-    alert("No input. Try again!");
-    return startGame();
-  }
-
-  if (!LETTER_REGEX.test(promptName)) {
-    alert("Your name should only contain letters. Please try again.");
-    return startGame();
-  }
-
-  // promptMayor starts
-  let promptMayor = prompt(`
-    🧙🏻‍♂️[Mayor]: Hi ${promptName}! Your grandfather left you his little farm. Also, he asked me to give you this letter.
-    💌
-    -----------------------------------------------
-    [0] Read letter
-    [1] Change your name`);
-
-  if (promptMayor === null) {
-    alert("You cancelled the process. Exiting game...");
-    return;
-  }
-
-  if (promptMayor.trim() === "") {
-    alert("No input. Try again!");
-    return startGame();
-  }
-
-  if (promptMayor === "1") {
-    return startGame();
-  } else if (promptMayor !== "0") {
-    alert("Invalid choice. Please try again.");
-    return startGame();
-  }
-
-  // promptLetter starts
-  let promptLetter = prompt(`
-    👴🏻[grandpa]: My dear ${promptName},                  
-    This farm is a gift to you, my beloved grandchild. 
-     But before you earn the ownership of the place, I want to test your knowledge first through a word guessing game.         
-    
-     You have ${leftAttempts} attempts to guess the correct word.
-     ✅ - right letter & right spot
-     🐥 - right letter but wrong spot
-     ⬛️ - wrong letter & wrong spot
-    
-     Give it your best shot, and the farm will be yours!,
-     Grandpa
-    -----------------------------------------------------
-   [0] Start game
-   [1] Change your name`);
-
-  // handle Cancel button
-  if (promptLetter === null) {
-    alert("You cancelled the process. Exiting game...");
-    return;
-  }
-  // handle OK button
-  if (promptLetter.trim() === "") {
-    alert("No input. Try again!");
-    return startGame();
-  }
-
-  if (promptLetter === "1") {
-    return startGame();
-  } else if (promptLetter !== "0") {
-    alert("Invalid choice. Please try again.");
-    return startGame();
-  }
-
+const playGame = () => {
   // promptCategory starts
   let selectedCategory;
-  while (true) { 
+  while (true) {
     let promptCategory = prompt(
       `🚩:::Harvest Hints Arena:::🚩
 
-    Please choose a category.
-    [1] Fruits & Vegetables 🥕🍌
-    [2] Farm Animals 🐷🦆
-    [3] Farm Products 🚜🌾🥛
-    `
+  Please choose a category.
+  [1] Fruits & Vegetables 🥕🍌
+  [2] Farm Animals 🐷🦆
+  [3] Farm Products 🚜🌾🥛
+  `
     );
 
     if (promptCategory === null) {
-      alert("You cancelled the process. Exiting game...");
+      alert("You are exiting the game...");
       return;
     }
 
     if (promptCategory.trim() === "") {
-      alert("No input. Try again!");
-      return startGame();
+      alert("No input. Please choose a category!");
+      continue;
     }
 
     switch (promptCategory) {
@@ -161,14 +81,14 @@ const startGame = () => {
     // promptUserGuess starts
     let promptUserGuess = prompt(
       `Category: ${selectedCategory[0]}
-      Guess the ${MAX_WORD_LENGTH}-letter word. [${leftAttempts} attempts left].
-      🐥🥦🥕🐮🥛🥩🌳
-      Attempt Summary:
-      ${attemptSummary}
-    `
+    Guess the ${MAX_WORD_LENGTH}-letter word. [${leftAttempts} attempts left].
+    🐥🥦🥕🐮🥛🥩🌳
+    Attempt Summary:
+    ${attemptSummary}
+  `
     ).toLowerCase();
     if (promptUserGuess === null) {
-      alert("You cancelled the process. Exiting game...");
+      alert("You are exiting the game...");
       return;
     }
 
@@ -195,20 +115,113 @@ const startGame = () => {
     );
 
     if (promptUserGuess === randomWord) {
-      alert(
+      let win = confirm(
         `👏Congratulations👏 
-        The correct word is ✨${randomWord}✨. 
-        You get the farm! 👴🏻🌳
-        🥳🥳🥳🥳🥳`
+      The correct word is ✨${randomWord}✨. 
+      You get the farm! 👴🏻🌳
+      🥳🥳🥳🥳🥳`
       );
       isGuessedCorrectly = true;
+      if (won) {
+        playGame();
+      } else {
+        alert("Thank you for playing!");
+      }
     } else if (leftAttempts === 0) {
-      alert(
+      let lose = confirm(
         `😭Game Over😭 
-          The correct word was "${randomWord}". 
-          Better luck next time! 👴🏻`
+        The correct word was "${randomWord}". 
+        Better luck next time! 👴🏻`
       );
+      if (lose) {
+        playGame();
+      } else {
+        alert("Thank you for playing!");
+      }
     }
   }
+};
+
+const startGame = () => {
+  // promptName starts
+  let promptName = prompt(
+    `🧙🏻‍♂️[Mayor]: Hi. My name is Thomas Morot, Mayor of Grönville. What is your name?`
+  );
+
+  if (promptName === null) {
+    alert("You are exiting the game...");
+    return;
+  }
+
+  if (promptName.trim() === "") {
+    alert("No input. Try again!");
+    return startGame();
+  }
+
+  if (!LETTER_REGEX.test(promptName)) {
+    alert("Your name should only contain letters. Please try again.");
+    return startGame();
+  }
+
+  // promptMayor starts
+  let promptMayor = prompt(`
+    🧙🏻‍♂️[Mayor]: Hi ${promptName}! Your grandfather left you his little farm. Also, he asked me to give you this letter.
+    💌
+    -----------------------------------------------
+    [0] Read letter
+    [1] Change your name`);
+
+  if (promptMayor === null) {
+    alert("You are exiting the game...");
+    return;
+  }
+
+  if (promptMayor.trim() === "") {
+    alert("No input. Try again!");
+    return startGame();
+  }
+
+  if (promptMayor === "1") {
+    return startGame();
+  } else if (promptMayor !== "0") {
+    alert("Invalid choice. Please try again.");
+    return startGame();
+  }
+
+  // promptLetter starts
+  let promptLetter = prompt(`
+    👴🏻[grandpa]: My dear ${promptName},                  
+    This farm is a gift to you, my beloved grandchild. 
+     But before you earn the ownership of the place, I want to test your knowledge first through a word guessing game.         
+    
+     You have ${leftAttempts} attempts to guess the correct word.
+     ✅ - right letter & right spot
+     🐥 - right letter but wrong spot
+     ⬛️ - wrong letter & wrong spot
+    
+     Give it your best shot, and the farm will be yours!,
+     Grandpa
+    -----------------------------------------------------
+   [0] Start game
+   [1] Change your name`);
+
+  // handle Cancel button
+  if (promptLetter === null) {
+    alert("You are exiting the game...");
+    return;
+  }
+  // handle OK button
+  if (promptLetter.trim() === "") {
+    alert("No input. Try again!");
+    return startGame();
+  }
+
+  if (promptLetter === "1") {
+    return startGame();
+  } else if (promptLetter !== "0") {
+    alert("Invalid choice. Please try again.");
+    return startGame();
+  }
+  playGame();
 };
 document.getElementById("startButton").addEventListener("click", startGame);
